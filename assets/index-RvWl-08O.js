cd51d6010035a3f6eb4a1e480bd41d00eee89bfd
front42 <altaifront@gmail.com>
2024-08-02 16:46:51 +0300

    Updates

---
+++ assets/index-RvWl-08O.js
@@ -0,0 +1,17 @@
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const s="/front-end-entrance-exam/javascript.svg",a="/front-end-entrance-exam/vite.svg";function l(c){let r=0;const o=n=>{r=n,c.innerHTML=`count is ${r}`};c.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${a}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${s}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello M¡ke!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;l(document.querySelector("#counter"));
