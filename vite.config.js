4b6dddebcdfb18b86d6a851f3d6b25cdb95fd215
front42 <altaifront@gmail.com>
2024-11-25 13:49:31 +0300

    feat: add browserslist

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 export default defineConfig({
   base: '/front-end-entrance-exam/',
+  build: {
+    target: browserslistToEsbuild(),
+  },
 });
